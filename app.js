8768784281d0b48e6a91e2a7325d852e97f4c6e1
Quentin <zli19@rrc.ca>
2023-11-06 11:05:28 -0600

    Add routes and controlles

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const taskRouter = require('./routes/taskRouter')
+
+// middleware
+app.use(express.json())
+
+// static asset
+app.use(express.static('./public'))
+
+// routes
+app.use('/api/v1/tasks', taskRouter)
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...')
+})
---
+++ controllers/taskController.js
@@ -0,0 +1,27 @@
+const getAllTasks = (req, res) => {
+    res.send('all items')
+}
+
+const createTask = (req, res) => {
+    res.send('all items')
+}
+
+const getTask = (req, res) => {
+    res.send('all items')
+}
+
+const updateTask = (req, res) => {
+    res.send('all items')
+}
+
+const deleteTask = (req, res) => {
+    res.send('all items')
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
